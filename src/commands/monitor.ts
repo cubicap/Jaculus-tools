52e1e0451cc08eeacddb6b49d8cc2fa688aea214
Petr Kubica <cubicap@email.cz>
2023-03-29 20:25:05 +0200

    add input to monitor

--- src/commands/monitor.ts
+++ src/commands/monitor.ts
@@ -1,25 +1,60 @@
-import { Command, Env } from "./lib/command.js";
+import { Command, Env, Opt } from "./lib/command.js";
 import { stdout } from "process";
 import { getDevice } from "./util.js";
 import chalk from "chalk";
+import readline from 'readline';
 
 
 let cmd = new Command("Monitor program output", {
-    action: async (options: Record<string, string | boolean>, args: Record<string, string>, env: Env) => {
-        let port = options["port"] as string;
-        let baudrate = options["baudrate"] as string;
-        let socket = options["socket"] as string;
+    action: (options: Record<string, string | boolean>, args: Record<string, string>, env: Env) => {
+        return new Promise(async (resolve, reject) => {
+            let echo = !(options["no-echo"] as boolean);
 
-        let device = await getDevice(port, baudrate, socket, env);
+            let rl = readline.createInterface({ input: process.stdin });
+            readline.emitKeypressEvents(process.stdin, rl);
+            process.stdin.setRawMode(true);
 
-        device.programOutput.onData((data) => {
-            stdout.write(data);
+            let port = options["port"] as string;
+            let baudrate = options["baudrate"] as string;
+            let socket = options["socket"] as string;
+
+            let device = await getDevice(port, baudrate, socket, env);
+
+            device.programOutput.onData((data) => {
+                stdout.write(data);
+            });
+            device.programError.onData((data) => {
+                stdout.write(chalk.red(data));
+            });
+
+            process.stdin.on('keypress', (str, key) => {
+                if (key.ctrl && key.name === 'c') {
+                    device.programOutput.onData((data) => { });
+                    device.programError.onData((data) => { });
+                    process.stdin.setRawMode(false);
+                    rl.close();
+                    resolve();
+                }
+                else {
+                    if (echo) {
+                        if (key.sequence === "\r") {
+                            stdout.write("\r\n");
+                        }
+                        else if (str) {
+                            stdout.write(str);
+                        }
+                    }
+                    let out = key.sequence;
+                    if (out === "\r") {
+                        out = "\n";
+                    }
+                    device.programInput.write(Buffer.from(out, "utf-8"));
+                }
+            });
         });
-        device.programError.onData((data) => {
-            stdout.write(chalk.red(data));
-        });
-
-        return new Promise((resolve, reject) => {});
+    },
+    options: {
+        "no-echo": new Opt("Echo input", { isFlag: true }),
     },
     chainable: true
 });
